c5e81c4d73cffa4b67bfe458dd7504408baf7198
lestra2 <lestra2@bancodebogota.com.co>
2025-04-07 19:48:21 -0500

    Create EKS

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "create" {
+  type = bool
+}
+variable "namespace" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "kubernetes_version" {
+  type = string
+}
+
+variable "enabled_cluster_log_types" {
+  type    = list(string)
+  default = []
+}
+
+variable "endpoint_private_access" {
+  type    = bool
+  default = false
+}
+
+variable "endpoint_public_access" {
+  type    = bool
+  default = true
+}
+
+variable "public_access_cidrs" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
